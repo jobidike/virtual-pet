f573f7449d23c61c259f2c47ffb07de2fa932849
jobidike <jobidike@gmail.com>
2017-10-06 15:18:46 -0400

    second commit

--- src/virtualpet/VirtualPet.java
+++ src/virtualpet/VirtualPet.java
@@ -1,7 +1,37 @@
 package virtualpet;
 
 public class VirtualPet {
+	//instance data
+	private int hunger;
+	private int waste;
+	private int boredom;
+	private int tiredness;
 	
-	//change
-
+	//constructor
+	public VirtualPet (int hunger, int waste, int boredom, int tiredness) {
+		this.hunger = hunger;
+		this.waste = waste;
+		this.boredom= boredom;
+		this.tiredness = tiredness;
+	}
+	
+	public int getHunger() {
+		return hunger;
+	}
+	public int getWaste() {
+		return waste;
+	}
+	public int getBoredom() {
+		return boredom;
+	}
+	public int getTiredness() {
+		return tiredness; 
+	}
+	
+	//method of behavior
+	
+	
+	
+	
 }
+	
--- src/virtualpet/VirtualPetApp.java
+++ src/virtualpet/VirtualPetApp.java
@@ -1,9 +1,21 @@
 package virtualpet;
-
+import java.util.Scanner;
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		VirtualPet tiger = new VirtualPet(30,15,10,50);
+		
+		System.out.println(tiger.getHunger());
+		System.out.println(tiger.getWaste());
+		System.out.println(tiger.getBoredom());
+		System.out.println(tiger.getTiredness());
+		
+		
+		
+		
+		
 
 	}
 
