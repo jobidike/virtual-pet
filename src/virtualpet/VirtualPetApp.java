cafeba95aea23f2075613fcc70533cf2448737d9
jobidike <jobidike@gmail.com>
2017-10-07 14:39:55 -0400

    third commit

--- src/virtualpet/VirtualPetApp.java
+++ src/virtualpet/VirtualPetApp.java
@@ -12,6 +12,20 @@ public class VirtualPetApp {
 		System.out.println(tiger.getBoredom());
 		System.out.println(tiger.getTiredness());
 		
+		System.out.println("Welcome to Virtual Pet World!");
+		String choice;
+		
+		do {
+			System.out.println("Choose something you would like to to with Timothy the Tiger!");
+			System.out.println("Press 1 to feed him");
+			System.out.println("Press 2 to take him to the bathroom ");
+			System.out.println("Press 3 to take him to the Tiger park at the Zoo");
+			System.out.println("Press 4 to give him a shot to put him to sleep");
+			System.out.println("Do nothing");
+			choice = input.nextLine();
+		}
+			while {
+		
 		
 		
 		
